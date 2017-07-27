18b8bb02b44bd1af681da65eb15bea5b8d931198
Matt Johnson <matt@primality.com>
2017-07-27 04:08:18 -0600

    Added BigDecimal retrieval.

--- dist/jest.config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  modulePathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/'],
-  moduleDirectories: ['node_modules'],
-  testRegex: 'specs/.*?(Spec)\\.js$'
-};
-//# sourceMappingURL=jest.config.js.map
